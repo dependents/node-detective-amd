6ea65faa9f3144715e408fc60d35dd16890eb9cc
XhmikosR <xhmikosr@gmail.com>
2023-04-25 15:02:13 +0300

    Switch to xo and rework tests (#26)

--- bin/cli.js
+++ bin/cli.js
@@ -1,20 +1,19 @@
 #!/usr/bin/env node
 
-/* eslint-disable no-console */
-
 'use strict';
 
 const fs = require('fs');
+const process = require('process');
 const getDependencies = require('../index.js');
 
 const filename = process.argv[2];
 
-if (!filename) {
+if (filename) {
+  const deps = getDependencies(fs.readFileSync(filename));
+  for (const dep of deps) {
+    console.log(dep);
+  }
+} else {
   console.log('Filename not supplied');
   console.log('Usage: detective-amd <filename>');
-} else {
-  const deps = getDependencies(fs.readFileSync(filename));
-  deps.forEach((dep) => {
-    console.log(dep);
-  });
 }
